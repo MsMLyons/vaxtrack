d3bb7b89559ccb7686e04c7fadd9f5c92128a037
MsMLyons <ms.markilyons@gmail.com>
2022-11-17 00:15:41 -0500

    Update app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import VaccineList from "./components/vaccineList";
+import CreateVaccine from "./components/createVaccine";
+import EditVaccine from "./components/editVaccine";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Routes>
+        <Route exact path="/" element={<VaccineList />} />
+        <Route path="/createVaccine" element={<CreateVaccine />} />
+        <Route path="/editVaccine" element={<EditVaccine />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
