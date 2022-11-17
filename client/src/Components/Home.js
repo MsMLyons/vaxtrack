99aa2d796e76cf90739081b32599371a2abfc688
techiefromva <sdp0186@gmail.com>
2022-11-17 17:31:43 -0500

    update home

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -5,7 +5,7 @@ import '../Style/Home.css'
 function Home() {
   return (
     <div className="home-container">
-       Vaxtrack serves as a repository for your vaccinations throughout yours or your child’s lifetime. This application allows the user to create individual accounts for themselves or family members for which they want to track their vaccination record. After creating a profile, they can enter vaccination records, upload important relevant documentation for each record, and even have a physician verify specific vaccinations. 
+       Vaxtrack serves as a repository for your vaccinations throughout your's or your child’s lifetime. This application allows the user to create individual accounts for themselves or family members for which they want to track their vaccination record. After creating a profile, they can enter vaccination records, upload important relevant documentation for each record, and even have a physician verify specific vaccinations. 
     </div>
 
     
