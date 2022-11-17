9551afcce299a01c5c599868471880966799573f
Agnes <agsawicka@gmail.com>
2022-11-17 14:58:48 -0500

    login & signup

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useState} from "react";
+import '../Style/Login.css'
+
 
 function Login() {
+    const[userName, setUserName]=useState("")
+    const[password, setPassword]=useState("")
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log("submitted");
+        console.log(userName, password)
+        setUserName('')
+        setPassword('')
+    }
+
   return (
-    <div>Login</div>
+    <div className="login-container">
+        Login
+        <img src='./VaxTrack.png' className='logo-login' alt="..."/>
+        <form onSubmit={handleSubmit}>
+            <div className="form-input-fields">
+            <label>
+            User name:
+            <input type="text" name="name" value={userName} onChange={(e)=> setUserName(e.target.value)}/>
+            </label>
+
+            <label>
+            Passowrd:
+            <input type="text" name="name" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+            </label>
+
+            <button>Login</button>
+            </div>
+        </form>
+        or
+        <button>SignUp </button>
+
+
+    </div>
   )
 }
 
--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, {useState} from "react";
 
 function SignUp() {
-  return (
-    <div>SignUp</div>
+    const[newUser, setNewUser]=useState({name:"", last_name: "",email: "",password:"", confirm_password:""})
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(newUser)
+    }
+    return (
+        <div className="login-container">
+        Signup
+        <img src='./VaxTrack.png' className='logo-login' alt="..."/>
+        <form onSubmit={handleSubmit}>
+            <div className="form-input-fields">
+            <label>
+            User name:
+            <input type="text" name="name" value={newUser.name} onChange={(e)=> setNewUser({...newUser, name: e.target.value})}/>
+            </label>
+            <label>
+            Last name:
+            <input type="text" name="last_name" value={newUser.last_name} onChange={(e)=> setNewUser({...newUser, last_name: e.target.value})}/>
+            </label>
+            <label>
+            Email:
+            <input type="text" name="last_name" value={newUser.email} onChange={(e)=> setNewUser({...newUser, email: e.target.value})}/>
+            </label>
+            <label>
+            Passowrd:
+            <input type="password" name="name" value={newUser.password} onChange={(e)=> setNewUser({...newUser, password: e.target.value})}/>
+            </label>
+
+            <label>
+            Confirm passowrd:
+            <input type="password" name="name" value={newUser.confirm_password} onChange={(e)=> setNewUser({...newUser, confirm_password: e.target.value})}/>
+            </label>
+
+            <button>Sign Up</button>
+            </div>
+        </form>
+        or
+        <button>Login</button>
+
+
+    </div>
   )
 }
 
