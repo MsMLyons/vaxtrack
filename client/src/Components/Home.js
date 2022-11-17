6d9ffd402e52f98b0eb41c557d10e206d360b111
Manish Patel <manishpatelapps@gmail.com>
2022-11-17 16:48:54 -0500

    update home, nav

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import '../Style/Home.css'
+
 
 function Home() {
   return (
-    <div>
-      
-      Home
+    <div className="home-container">
+       Vaxtrack serves as a repository for your vaccinations throughout yours or your child’s lifetime. This application allows the user to create individual accounts for themselves or family members for which they want to track their vaccination record. After creating a profile, they can enter vaccination records, upload important relevant documentation for each record, and even have a physician verify specific vaccinations. 
     </div>
+
+    <div className="home-footer"
+
+
   )
 }
+
+
 
 export default Home
