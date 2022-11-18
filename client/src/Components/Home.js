88a8356b6290f96cb7534bf228efa01c34d7be21
techiefromva <sdp0186@gmail.com>
2022-11-18 11:59:29 -0500

    update home

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,16 +2,16 @@ import React from 'react'
 import '../Style/Home.css'
 
 
+
 function Home() {
   return (
   <div className="home-container">
-      <div className="text-main">Vaxtrack serves as a repository for your vaccinations throughout yours or your child’s lifetime.
-        This application allows the user to create individual accounts for themselves or family members for which they
-        want to track their vaccination record. After creating a profile, they can enter vaccination records, upload important
-        relevant documentation for each record,
-        and even have a physician verify specific vaccinations. </div>
+      <div className="text-main">Keep track of your vaccinations easily with Vaxtrack! We are an easy to use platform that lets you create individual accounts for yourself or family members to monitor your vaccination record over time. No more lost vaccine records at the start of each new job or school year…. After creating a user profile, enter vaccine history and important relevant documentation. Even have your physician verify specific vaccinations via our system! 
+      
+      Try VaxTrack Now! 
+       </div>
       <div className="picture-main">
-      add picture here
+        <img className="img-mainpage" src='./HomePic.png' alt="HomePic" />
       </div>
     </div>  
 
