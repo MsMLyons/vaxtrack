ee71154b9033997fcc21e7a75ce6c0452b94bbdd
MsMLyons <ms.markilyons@gmail.com>
2022-11-19 11:17:34 -0500

    Update home page text

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <div className="home-container">
       
       <div className="text-main">
-        <h5>FAMILY VACCINATION RECORD IN ONE PLACE</h5>
+        <h5>FAMILY VACCINATION RECORDS IN ONE PLACE</h5>
         <br/>
         Vaxtrack serves as a repository for your family vaccination records so that you have access to them when it matters most. 
         This application allows the user to create individual accounts for themselves as well as family members to track vaccination records. 
