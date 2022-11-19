3d394281edeb1d89faacb5613d2ab643ea899e60
Agnes <agsawicka@gmail.com>
2022-11-18 21:51:01 -0500

    home page

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -6,15 +6,16 @@ function Home() {
   return (
     <div className="home-container">
       
-      <div className="text-main">Vaxtrack serves as a repository for your vaccinations throughout yours or your child’s lifetime. 
+      <div className="text-main">
+        <h5>FAMILY VACCINATION RECORD IN ONE PLACE</h5>
+        <br/>
+        Vaxtrack serves as a repository for your vaccinations throughout yours or your child’s lifetime. 
         This application allows the user to create individual accounts for themselves or family members for which they 
-        want to track their vaccination record. After creating a profile, they can enter vaccination records, upload important 
-        relevant documentation for each record, 
-        and even have a physician verify specific vaccinations. </div>
+        want to track their vaccination record.  </div>
       
       <div className="picture-main">
 
-      add picture here
+        <img src="./main.png" id="main-page-img" alt=".."/>
 
       </div>
 
